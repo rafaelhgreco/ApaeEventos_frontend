183cfd7c200b204813f97ef7ffae5ea450218641
rafaelhgreco <zrafaelgreco@gmail.com>
2025-10-18 19:41:12 -0300

    refactor: ajustes na pagina de gerenciamento de eventos

--- services/report_services.ts
+++ services/report_services.ts
@@ -1,6 +1,5 @@
+import { API_BASE_URL } from "@env";
 import axios from "axios";
-
-const API_URL = "http://35.247.231.143:3000"; // atualize se necessário
 
 export interface ReportData {
     total: number;
@@ -11,7 +10,7 @@ export const getEventReport = async (
     eventId: string,
     token: string
 ): Promise<ReportData> => {
-    const response = await axios.get(`${API_URL}/report/${eventId}`, {
+    const response = await axios.get(`${API_BASE_URL}/report/${eventId}`, {
         headers: {
             Authorization: `Bearer ${token}`,
         },
